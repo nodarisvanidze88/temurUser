929c937f4accef14727d62a3423451baf328ec8f
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-07-31 02:05:24 +0400

    modified filter

--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,7 @@ from .models import TemurUsers, Courses
 
 class TemurAdmin(admin.ModelAdmin):
     list_display = ("name","tel","email","registration","course")
-    list_filter = ("name","tel","email","registration","course")
+    list_filter = ("registration","course")
     search_fields = ("name","tel","email","registration","course")
 admin.site.register(TemurUsers, TemurAdmin)
 admin.site.register(Courses)
